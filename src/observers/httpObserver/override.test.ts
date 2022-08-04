3f10938dc9c8363e31ee65a84a2addad70817fc1
Jeremy Giberson <jeremy.giberson@formidable.com>
2022-08-04 15:07:13 -0700

    chore: refining and testing http/https module override functionality

--- src/observers/httpObserver/override.test.ts
+++ src/observers/httpObserver/override.test.ts
@@ -1,12 +1,27 @@
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
 import http from 'http';
 import https from 'https';
-import { override, EVENT } from './override';
+import {
+  override,
+  EVENT_REQUEST,
+  RequestArgs,
+  RequestEventArgs,
+  ResponseEventArgs,
+  EVENT_RESPONSE,
+} from './override';
 import EventEmitter from 'events';
 import { Stubby } from 'stubby';
 
-type TestCases = Array<[string, typeof http | typeof https, string]>;
-
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+type TestCases = Array<
+  [
+    string,
+    typeof http | typeof https,
+    Partial<RequestArgs>,
+    Partial<RequestEventArgs>,
+    Partial<ResponseEventArgs>,
+  ]
+>;
 
 describe('httpObserver/override', () => {
   const emitter = new EventEmitter();
@@ -14,8 +29,73 @@ describe('httpObserver/override', () => {
   override(https, emitter);
 
   const tests: TestCases = [
-    ['http', http, 'http://localhost:8882/'],
-    ['https', https, 'https://localhost:7443/'],
+    [
+      'http.request(url)',
+      http,
+      ['http://localhost:8882/'],
+      { method: 'GET', port: '8882', host: 'localhost' },
+      { statusCode: 200 },
+    ],
+    // Can't test url only because we have to provide rejectUnauthorized: false for self sign cert of stub
+    // [
+    //   'https.request(url)',
+    //   https,
+    //   ['https://localhost:7443/', { rejectUnauthorized: false }],
+    //   { method: 'GET', port: '7443', host: 'localhost' },
+    // ],
+    [
+      'http.request(options)',
+      http,
+      [{ method: 'GET', host: 'localhost', protocol: 'http:', port: '8882', path: '/' }],
+      { method: 'GET', port: '8882', host: 'localhost', path: '/' },
+      { statusCode: 200 },
+    ],
+    [
+      'https.request(options)',
+      https,
+      [
+        {
+          method: 'GET',
+          host: 'localhost',
+          protocol: 'https:',
+          port: '7443',
+          path: '/',
+          rejectUnauthorized: false,
+        },
+      ],
+      { method: 'GET', port: '7443', host: 'localhost' },
+      { statusCode: 200 },
+    ],
+    [
+      'http.request(url, options)',
+      http,
+      ['http://localhost:8882/', { method: 'OPTIONS' }],
+      { method: 'OPTIONS', port: '8882', host: 'localhost' },
+      { statusCode: 200 },
+    ],
+    [
+      'https.request(url, options)',
+      https,
+      ['https://localhost:7443/', { method: 'OPTIONS', rejectUnauthorized: false }],
+      { method: 'OPTIONS', port: '7443', host: 'localhost' },
+      { statusCode: 200 },
+    ],
+    [
+      'http.request(url, options) => 500 response',
+      http,
+      ['http://localhost:8882/error', { method: 'OPTIONS' }],
+      { method: 'OPTIONS', port: '8882', host: 'localhost' },
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      { statusCode: 500 },
+    ],
+    [
+      'https.request(url, options) => 500 response',
+      https,
+      ['https://localhost:7443/error', { method: 'OPTIONS', rejectUnauthorized: false }],
+      { method: 'OPTIONS', port: '7443', host: 'localhost' },
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      { statusCode: 500 },
+    ],
   ];
 
   const stubby = new Stubby();
@@ -26,7 +106,22 @@ describe('httpObserver/override', () => {
         tls: 7443,
         data: [
           {
-            request: { url: '/' },
+            request: { url: '/$', method: 'GET' },
+          },
+          {
+            request: { url: '/$', method: 'OPTIONS' },
+          },
+          {
+            request: { url: '/error', method: 'GET' },
+            response: {
+              status: 500,
+            },
+          },
+          {
+            request: { url: '/error', method: 'OPTIONS' },
+            response: {
+              status: 500,
+            },
           },
         ],
       },
@@ -41,48 +136,41 @@ describe('httpObserver/override', () => {
     });
   }, 1000);
 
-  it('stub', (done) => {
-    const req = https.request(
-      'https://localhost:7443/',
-      {
-        rejectUnauthorized: false,
-      },
-      (res) => {},
-    );
-    req.end(() => {
-      done();
-    });
-    req.on('error', (err) => {
-      console.error(err);
-      done(err);
-    });
-  }, 30000);
-
   tests.forEach((test) => {
     const scenario = test[0];
     const mod = test[1];
-    const url = test[2];
+    const args = test[2];
+    const expectedRequestEventArgs = test[3];
+    const expectedResponseEventArgs = test[4];
     describe(`${scenario}`, () => {
-      it('emits on request', function (done) {
-        const cb = jest.fn();
-        emitter.once(EVENT, cb);
-        const req = mod.request(
-          url,
-          {
-            rejectUnauthorized: false,
-          },
-          (res) => {},
-        );
-        req.end(() => {
-          expect(cb).toHaveBeenCalled();
-          done();
+      it('emits request and response events', function (done) {
+        const requestEventCallback = jest.fn();
+        const responseEventCallback = jest.fn();
+        emitter.once(EVENT_REQUEST, requestEventCallback);
+        emitter.once(EVENT_RESPONSE, responseEventCallback);
+
+        // @ts-ignore
+        const req = mod.request(...args);
+        req.on('close', () => {
+          try {
+            expect(requestEventCallback).toHaveBeenCalledWith(
+              expect.objectContaining(expectedRequestEventArgs),
+            );
+            expect(responseEventCallback).toHaveBeenCalledWith(
+              expect.objectContaining(expectedResponseEventArgs),
+            );
+            done();
+          } catch (err) {
+            done(err);
+          }
         });
-        req.on('error', (err) => {
-          done(err);
+
+        req.on('response', (response) => {
+          // data must be consumed for end event to fire
+          response.on('data', (_chunk) => {});
         });
+        req.end();
       }, 1000);
-      it.todo('emits on response error');
-      it.todo('emits on response end');
     });
   });
 });
--- src/observers/httpObserver/override.ts
+++ src/observers/httpObserver/override.ts
@@ -1,78 +1,124 @@
 import http from 'http';
 import https, { RequestOptions } from 'https';
 import EventEmitter from 'events';
+import { URL } from 'node:url';
 
-export const EVENT = 'watched';
+export const EVENT_REQUEST = 'request';
+export const EVENT_RESPONSE = 'response';
+export type RequestEventArgs = {
+  connectionID: string;
+  method: string;
+  host: string;
+  port: string;
+  path?: string;
+  headers: Record<string, any>;
+};
+
+export type ResponseEventArgs = {
+  connectionID: string;
+  statusCode: number;
+  time: number;
+  headers: Record<string, string>;
+  httpVersion: string;
+  statusMessage: string;
+  error?: Error;
+};
+
 let uniqID = 0;
+
+/**
+ * url can be a string or a URL object. If url is a string, it is automatically parsed with new URL(). If it is a URL object, it will be automatically converted to an ordinary options object.
+ *
+ * If both url and options are specified, the objects are merged, with the options properties taking precedence.
+ */
+export function requestOptions(
+  urlOrOpts: string | URL | RequestOptions,
+  optionsOrCallback: RequestOptions | ((res: http.IncomingMessage) => void),
+  callback?: (res: http.IncomingMessage) => void,
+) {
+  let url: Partial<URL> = {};
+  if (typeof urlOrOpts === 'string') {
+    url = new URL(urlOrOpts);
+  } else if (urlOrOpts instanceof URL) {
+    url = urlOrOpts;
+  } else if (typeof urlOrOpts === 'object') {
+    // handled later
+  } else if (typeof optionsOrCallback !== 'object') {
+    // TODO maybe return {} instead
+    // @ts-ignore
+    // eslint-disable-next-line prefer-rest-params
+    throw new Error(
+      'invalid request options' + JSON.stringify({ urlOrOpts, optionsOrCallback, callback }),
+    );
+  }
+
+  const urlOpts = {
+    href: url.href,
+    origin: url.origin,
+    protocol: url.protocol,
+    username: url.username,
+    password: url.password,
+    host: url.host,
+    hostname: url.hostname,
+    port: url.port,
+    pathname: url.pathname,
+    search: url.search,
+    searchParams: url.searchParams,
+    hash: url.hash,
+  };
+
+  return Object.assign(
+    {},
+    urlOpts,
+    typeof urlOrOpts === 'object' ? urlOrOpts : {},
+    typeof optionsOrCallback === 'object' ? optionsOrCallback : {},
+  );
+}
+
+export type RequestArgs = [
+  urlOrOpts: string | URL | RequestOptions,
+  optionsOrCallback: RequestOptions | ((res: http.IncomingMessage) => void),
+  callback?: (res: http.IncomingMessage) => void,
+];
 
 export function override(module: typeof http | typeof https, emitter: EventEmitter) {
   const original = module.request;
 
   // @ts-ignore
-  function wrapper(...args) {
+  function wrapper(...args: RequestArgs) {
     const connectionID = `rq:${++uniqID}`;
     // @ts-ignore
     const req = original.apply(this, args);
-    // eslint-disable-next-line @typescript-eslint/unbound-method
-    const originalEmit = req.emit;
 
-    // @ts-ignore
-    req.emit = function emit(eventName: string | symbol, response: http.IncomingMessage, ...rest) {
-      switch (eventName) {
-        case 'response': {
-          response.once('error', (e) => {
-            const res = {
-              connectionID,
-              statusCode: response.statusCode,
-              time: Date.now(),
-              headers: response.headers || {},
-              httpVersion: response.httpVersion,
-              statusMessage: response.statusMessage,
-              error: e,
-            };
-            emitter.emit(EVENT, res);
-          });
-          response.once('end', () => {
-            const res = {
-              connectionID,
-              time: Date.now(),
-              httpVersion: response.httpVersion,
-              statusCode: response.statusCode,
-              headers: response.headers || {},
-              statusMessage: response.statusMessage,
-            };
-            emitter.emit(EVENT, res);
-          });
-        }
-      }
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      return originalEmit.apply(this, [eventName, response, ...rest]);
-    };
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    logger(args[0], connectionID, req);
-    return req;
-  }
-
-  function logger(options: RequestOptions, connectionID: string, req: http.ClientRequest) {
-    // eslint-disable-next-line @typescript-eslint/unbound-method
-    const origEnd = req.end;
-    // @ts-ignore
-    req.end = function end(...args) {
+    req.on('response', (response: http.IncomingMessage) => {
+      response.on('end', () => {
+        const res = {
+          connectionID,
+          time: Date.now(),
+          httpVersion: response.httpVersion,
+          statusCode: response.statusCode,
+          headers: response.headers || {},
+          statusMessage: response.statusMessage,
+        };
+        emitter.emit(EVENT_RESPONSE, res);
+      });
+    });
+    req.on('close', () => {
+      const options = requestOptions(...args);
       const log = {
         connectionID,
         time: Date.now(),
         method: options.method || 'GET',
-        isRequest: true,
-        host: options.host || options.hostname || 'localhost',
-        port: options.port || '443',
-        path: options.path || '/',
+        host: options.hostname || options.host,
+        port: options.port,
+        path: options.path,
         headers: options.headers || {},
       };
-      emitter.emit(EVENT, log);
-      // @ts-ignore
-      return origEnd.apply(this, args);
-    };
+      emitter.emit(EVENT_REQUEST, log);
+    });
+    return req;
   }
 
+  // @ts-ignore
   module.request = wrapper;
 }
